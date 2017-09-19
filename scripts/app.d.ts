d4adbaa1b57438b26ec57397e6d3945025a64cb8
Tomáš Vicherek <vicherek@atlantis.cz>
2017-09-19 13:11:48 +0200

    První commit. V Runtime je ale objekt LineChart undefined.

---
+++ scripts/app.d.ts
@@ -0,0 +1,14 @@
+/// <reference path="react-d3.d.ts" />
+declare class Graf extends React.Component<{}, {}> {
+    render(): JSX.Element;
+}
+declare function RenderGraph(): void;
+declare function getData(): {
+    "name": string;
+    "BMI": number;
+    "age": number;
+    "birthday": string;
+    "city": string;
+    "married": boolean;
+    "index": number;
+}[];
---
+++ scripts/react-d3.d.ts
@@ -0,0 +1,25 @@
+﻿
+interface ChartProps {
+    title: any;
+    width: any;
+    height: any;
+    margins: any;
+}
+
+declare class Chart extends React.Component<ChartProps, {}> {
+
+}
+
+interface LineChartProps {
+    margins: any;
+    title: string;
+    data: any[];
+    width: number;
+    height: number;
+    chartSeries: any;
+    x(any): void;
+}
+
+declare class LineChart extends React.Component<LineChartProps, {}> {
+
+}
